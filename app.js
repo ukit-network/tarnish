bc4dd70c1eb31fd6a0377b3c919597f4060f3eb2
Dave Williamson <dave@ukit.net>
2013-07-17 22:26:40 +0200

    Update to logging and startup routines

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
+require('./lib/extendJS');
+var config = require('./lib/config');
+config.root = __dirname;
 
-/**
- * Module dependencies.
- */
-require('./lib/extendJS'); 
-var log = require('./lib/logging').newLogger(module.id)
+var log = require('./lib/logging').newLogger(__dirname+'/app.js')
   , config = require('./lib/config')
   , express = require('express')
   , routes = require('./routes')
@@ -12,25 +11,31 @@ var log = require('./lib/logging').newLogger(module.id)
   , security = require('./lib/security')
   , util = require('util')
   , http = require('http')
-  , path = require('path')
   , immediately = global.setImmediate || process.nextTick
 ;
 
 var app = express();
 
-log.warn("System starting...");
+
+log.info("System starting...");
 
 app.configure(function(){
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'hjs');
+  if(config.trustProxy){
+	  log.warn("Configured for using frontend proxy, 'x-forwarded-for' will be trusted");
+	  app.enable('trust proxy');
+  }
+  if(config.logTimes){
+	  log.warn("Configured to log timings, this will degrade performance");
+	  app.use(express.responseTime())
+  }
+  app.use(express.static(config.root + '/public'));
+  app.use(require('less-middleware')({ src: config.root + '/public' }));
+  app.set('views', config.root + '/views');
+  app.set('view engine', 'html');
+  app.engine('html', require('hogan-express'));
   app.use(express.favicon());
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
-  app.use(express.session());
-  app.use(app.router);
-  app.use(require('less-middleware')({ src: __dirname + '/public' }));
-  app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.configure('development', function(){
@@ -39,11 +44,12 @@ app.configure('development', function(){
 	app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
 
 
 var checkStart = function(){
-	if(dataStore.isReady){
+	
+	if(dataStore.isReady()){
+		
 		ptools.started(function(){
 		
 			// Comes in here on exit to cleanly shutdown application
@@ -57,18 +63,48 @@ var checkStart = function(){
 		});
 		
 		dataStore.configLoad(function(err, status){
+			
 			if(!err & status){
-				log.warn("Config loaded OK");
+				log.info("Config loaded OK");
 			} else {
 				log.error("Config NOT loaded - could be first execution: "+util.inspect(err, {colors: true, showHidden: true, maxDepth: 5}));
 			}
+			
+			if(config.data.useSessions){
+				log.warn("Server configured to provide sessions");
+				app.use(express.cookieParser(config.data.cookieSecret));
+				app.use(express.session({
+					  key : config.data.cookieId
+					, secret : config.data.cookieSecret
+					, cookie : {
+						  path : '/'
+						, httpOnly : true
+						, maxAge : config.data.cookieAge
+					  }
+					
+					, store : new sessionStore({
+						  client : datastore.getRedisClient()
+						, db : config.data.sessionDatabase
+						, prefix : config.data.sessionPrefix
+					})
+				}));
+			} else {
+				log.warn("Server configured NOT to provide sessions");	
+			}
+			
+			app.use(app.router);			
+			
+			app.all('*', routes.index);
+			
 			log.info("Starting server " + config.instanceData.serverPort  + ' - environment: ' + app.get('env'));
+			
 			http.createServer(app).listen(config.instanceData.serverPort, function () {
-				security.downgradeExecution(config.instanceData.user,config.instanceData.group);
 				http.globalAgent.maxSockets = config.data.maxHttpSockets;
+				security.downgradeExecution(config.instanceData.user,config.instanceData.group);
 				log.warn(config.getVersion());
 				log.warn("Server listening on port " + config.instanceData.serverPort  + ' - environment: ' + app.get('env'));
 				log.warn("This servers identifier: "+config.getServerId());
+				
 				log.info("Saving current config...");
 				dataStore.configSave(function(err, status){
 					if(err || !status){
@@ -84,4 +120,5 @@ var checkStart = function(){
 		immediately(checkStart);
 	}
 };
+dataStore.init();
 checkStart();
